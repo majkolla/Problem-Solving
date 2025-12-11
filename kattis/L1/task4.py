fdf90abaffe002d748466f7e6f49d7938f5ed113
majkolla <michael.birtman@gmail.com>
2025-12-11 09:48:58 +0100

    made a dataype

--- kattis/L1/task4.py
+++ kattis/L1/task4.py
@@ -14,6 +14,25 @@ we are going to be using union find
 - union-by-size   (attach the smaller tree under the larger one)
 - path compression (flattens paths during find)
 
+data structure:
+
+we represent each set as a rooted tree so: 
+
+parant[x] = paraent of x in the tree
+a root r satisfies parent[r] = r and is representing the complete set
+size[r] is the number of elements in the ree rooted at r 
+
+first the find(x) follows parents until it gets to the root of the tree of x. That root 
+is the id of the set of x. 
+
+we do this by: 
+union by size: 
+here we can just connect the smaller tree under the larger one. Therefore, 
+we have it so taht the larger trees root is the root of the combined tree. 
+
+path compression: 
+during find(x) we make the path shorter from x to the root by making 
+every node on the path point directly to the node. Then we get a posisbly flat tree or almost flat
 
 Complexities
 - time : O((N + Q) · (N))  here o is the ackerman function that grows very slowly so we are basically dealing with a constant 
@@ -22,56 +41,68 @@ so we get: - O(N + Q)
 
 
 """
+
+
 import sys
 
-def find(x: int, parent : list[int]) -> int:
-    """Return the root of the set containing x"""
-    
-    while parent[x] != x:
-    
-        parent[x] = parent[parent[x]]     
-        x = parent[x]
-    
-    return x
+
+class DisjointSetUnion:
+    def __init__(self, n: int) -> None:
+        self.parent = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x: int) -> int:
+        # path halving/compression 
+        while self.parent[x] != x:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+
+    def union(self, a: int, b: int) -> bool:
+        ra = self.find(a)
+        rb = self.find(b)
+        if ra == rb:
+            return False
+
+        # Union by size! ra is root of the larger tree
+        if self.size[ra] < self.size[rb]:
+            ra, rb = rb, ra
+
+        self.parent[rb] = ra
+        self.size[ra] += self.size[rb]
+        return True
+
+    def same(self, a: int, b: int) -> bool:
+        return self.find(a) == self.find(b)
+
 
 
 def solve() -> None:
     data = sys.stdin.read().split()
-    if not data:
-        return
+
 
     n = int(data[0])
     q = int(data[1])
 
-    # union find init 
-    parent: list[int] = list(range(n))
-    size:list[int] = [1] * n          # valid at roots
+    dsu = DisjointSetUnion(n)
+    out_lines = []
 
-
-    out_lines: list[str] = []
-    idx = 2  # current reading position in data
-
+    idx = 2
     for _ in range(q):
-        op = data[idx]          
+        op = data[idx]
         a = int(data[idx + 1])
         b = int(data[idx + 2])
         idx += 3
 
-        if op == '=':          # union
-            ra = find(a, parent)
-            rb = find(b, parent)
-            if ra != rb:        # merge smaller under larger
-                if size[ra] < size[rb]:
-                    ra, rb = rb, ra
-                parent[rb] = ra
-                size[ra] += size[rb]
-        else:
-            if find(a, parent) == find(b, parent):
+        if op == '=':
+            dsu.union(a, b)
+        elif op == '?':
+            if dsu.same(a,b):
                 out_lines.append("yes")
-            else: 
+            else:
                 out_lines.append("no")
-                
+
     sys.stdout.write("\n".join(out_lines))
 
-
 solve()
+

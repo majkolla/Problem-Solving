fae28cd653bb4169b6b228be849fe7225326a4ee
majkolla <michael.birtman@gmail.com>
2025-12-11 09:24:07 +0100

    added comments

--- kattis/L1/task1.py
+++ kattis/L1/task1.py
@@ -4,6 +4,34 @@
  start with the set that starts at (or before) A and cover the most ground. 
 
  We first sort the smallest a then the second priority is the biggest b if they have the same a (or the biggest abs(a-b))
+
+
+----- improvements: 
+
+the idea is to go through the seq. and keep the info of the best increasing seq we seen
+up to that point. 
+
+tails_val[k] - smallest possible end val of any increasing subseq. of len k+1
+tails_index  - index of the orginal array where the end val is 
+pred[i] - idx of the prev. elemetn in the subseq. that ends at pos i. 
+
+
+For each element x at i we do: 
+find bisect left, the first position in tails val where cx could be placed 
+while keeping tails_val sorted 
+
+if pos = current length of tails_value, we extend the
+subseq. by one => append x
+
+if pos > 0 the previous element of i is the index stored in tail_index[pos-1] 
+bc. the idx ends at an optimal subseq. with len pos that x extends to len pos +1. 
+
+finally:
+the len of tails_val is the len of a LIS 
+the laöst idx of that LIS is tail_index[-1] 
+and then we reconstruct the LIS idxs by doing pred[] backwards from last idx and 
+reversing the indxs we got.  
+
 """
 
 import sys 
